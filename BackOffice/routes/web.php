3bb766b10c673240556a488504d4d50a1c8d5e6b
JoaoCarrapato <carrapato.95@gmail.com>
2017-07-20 18:19:31 +0100

    laravel

---
+++ BackOffice/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+/*irrelevante*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+/*pagina inicial*/
+Route::get('/', 'PagesController@index')->name('index');
+
+
+
+	Route::resource('index','PagesController');
+	Route::resource('curso', 'CursoControllerBack');
+    Route::resource('disciplina', 'DisciplinaControllerBack');
+    Route::resource('horario', 'HorarioControllerBack');
+    Route::resource('user', 'UsersControllerBack');
+    Route::resource('utilizador', 'UtilizadoresControllerBack');
+    Route::resource('evento', 'EventoControllerBack');
+    Route::resource('documento', 'DocumentoControllerBack');
+    Route::resource('financa', 'FinancasControllerBack');
+
+Route::group(array('prefix' => 'api'), function() {
+	Route::resource('cursos', 'CursoController');
+	Route::resource('horarios', 'HorarioController');
+	Route::resource('eventos', 'EventoController');
+	Route::resource('financas', 'FinancaController');
+	Route::resource('documentos', 'DocumentoController');
+});
